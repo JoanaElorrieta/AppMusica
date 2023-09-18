eb2a1105bd97d19c6e6585bc61f4e22f3789eb71
Joana <joana.barbermo@elorrieta-errekamari.com>
2023-09-18 17:10:54 +0200

    Play pause funciona
    Cambios en layout y funcion play pause

--- app/src/main/java/com/example/appmusica/MainActivity.kt
+++ app/src/main/java/com/example/appmusica/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.appmusica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        val buttonPlay: ImageButton = findViewById(R.id.play)
+        val buttonPause: ImageButton = findViewById(R.id.pause)
+
+
+        buttonPlay.setOnClickListener {
+            buttonPlay.visibility=View.INVISIBLE
+            buttonPause.visibility=View.VISIBLE
+        }
+        buttonPause.setOnClickListener {
+            buttonPause.visibility=View.INVISIBLE
+            buttonPlay.visibility=View.VISIBLE
+        }
+
+        }
 }
+
