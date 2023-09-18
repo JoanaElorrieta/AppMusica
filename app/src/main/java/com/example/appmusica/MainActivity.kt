1f3c6d98f0c736bd559cccef62cfad7d859d5f95
Joana <joana.barbermo@elorrieta-errekamari.com>
2023-09-18 21:02:35 +0200

    App mantiene estado de reproduccion cambiando la orientacion de la pantalla

--- app/src/main/java/com/example/appmusica/MainActivity.kt
+++ app/src/main/java/com/example/appmusica/MainActivity.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
+    private var isPlaying=false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,12 +18,38 @@ class MainActivity : AppCompatActivity() {
         buttonPlay.setOnClickListener {
             buttonPlay.visibility=View.INVISIBLE
             buttonPause.visibility=View.VISIBLE
+            isPlaying=true
         }
         buttonPause.setOnClickListener {
             buttonPause.visibility=View.INVISIBLE
             buttonPlay.visibility=View.VISIBLE
+            isPlaying=false
         }
 
         }
+    override fun onResume() {
+        super.onResume()
+        val buttonPlay: ImageButton = findViewById(R.id.play)
+        val buttonPause: ImageButton = findViewById(R.id.pause)
+
+        if(isPlaying){
+            buttonPlay.visibility=View.INVISIBLE
+            buttonPause.visibility=View.VISIBLE
+        }else{
+            buttonPause.visibility=View.INVISIBLE
+            buttonPlay.visibility=View.VISIBLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("estado",isPlaying)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        isPlaying=savedInstanceState.getBoolean("estado")
+
+    }
 }
 
